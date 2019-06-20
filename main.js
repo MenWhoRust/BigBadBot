24c8572d6a795c948450273013a76b3c32695681
Phabiann-Ross De Carvalho <prdecarvalho96@gmail.com>
2019-06-20 20:32:56 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import env from './env';
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready');
+});
+
+client.login(env.key);
